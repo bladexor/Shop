f97fff958e75d6ee52e6765568f2f2020af3d684
Bladimir Velásquez <bladi135@gmail.com>
2019-05-17 09:43:15 -0430

    Web Login Logout

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -18,5 +18,24 @@ namespace Shop.Web.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2");
+
+            var cascadeFKs = builder.Model
+                .GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+            foreach(var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(builder);
+        }
     }
 }
