f5a72718f5777ab6069375b5f4237173ac4a9e81
Bladimir Velásquez <bladi135@gmail.com>
2019-05-18 15:51:00 -0430

    View Create Order, and Add Product To Order

--- Shop.Web/Data/Repositories/IProductRepository.cs
+++ Shop.Web/Data/Repositories/IProductRepository.cs
@@ -1,4 +1,5 @@
-﻿using Shop.Web.Data.Entities;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Shop.Web.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,7 @@ namespace Shop.Web.Data
     public interface IProductRepository:IGenericRepository<Product>
     {
         IQueryable GetAllWithUsers();
+
+        IEnumerable<SelectListItem> GetComboProducts();
     }
 }
